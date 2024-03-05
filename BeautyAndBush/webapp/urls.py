6bd9a5bbebbb9f96753db66c0c7ce5505d377943
Shreeya Maskey <shreeyamaskey.1@gmail.com>
2024-03-04 21:57:12 -0700

    set up basic wireframe structure

--- BeautyAndBush/webapp/urls.py
+++ BeautyAndBush/webapp/urls.py
@@ -4,5 +4,13 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('moodboard/', views.MoodBoardView, name='moodboard')
+    path('moodboard/', views.MoodBoardView, name='moodboard'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('memberships/', views.memberships, name='memberships'),
+    path('contact-us/', views.contact_us, name='contact_us'),
+    path('login/', views.login, name='login'),
+    path('my-account/', views.my_account, name='my-account'),
+    path('history/', views.history, name='history')
 ]
