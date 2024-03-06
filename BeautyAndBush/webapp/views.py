4f0626966b8a2211e15bc28c1efde43dd97c478d
gencopey <willmercer28@yahoo.com>
2024-03-06 12:21:15 -0700

    hh
    hh

--- BeautyAndBush/webapp/views.py
+++ BeautyAndBush/webapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+<<<<<<< Updated upstream
 # Create your views here.
+=======
+def MoodBoardView(request):
+    return render(request, 'webapp/moodboard.html')
+
+
+>>>>>>> Stashed changes
