22c039efc19ac6c5fca8dfccf918fc39a2ec9fc1
Taeden Anderson <taet.anderson@gmail.com>
2024-03-20 12:41:04 -0600

    small change teehee

--- BeautyAndBush/webapp/models.py
+++ BeautyAndBush/webapp/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models import Model
 from django.shortcuts import render
 
 
@@ -7,7 +6,7 @@ def MoodBoardView(request):
     return render(request, 'webapp/moodboard.html')
 
 
-def MyAccountView(Model):
+def MyAccountView():
     name = models.CharField(max_length=200)
     email = models.EmailField(max_length=200)
     purchase_history = models.TextField(max_length=500)
