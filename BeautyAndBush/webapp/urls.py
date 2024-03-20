a2f752ab35fe6634021eb253633c192fa5575dc6
gencopey <willmercer28@yahoo.com>
2024-03-20 13:07:52 -0600

    NEW

--- BeautyAndBush/webapp/urls.py
+++ BeautyAndBush/webapp/urls.py
@@ -1,8 +1,22 @@
 from . import views
 
+
 from django.urls import path
 
 
+
+
 urlpatterns = [
-    path('moodboard/', views.MoodBoardView, name='moodboard'),
+   path('moodboard/', views.MoodBoardView, name='moodboard'),
+   path('', views.index, name='index'),
+   path('about/', views.about, name='about'),
+   path('services/', views.services, name='services'),
+   path('memberships/', views.memberships, name='memberships'),
+   path('contact-us/', views.contact_us, name='contact_us'),
+   path('login/', views.login, name='login'),
+   path('my-account/', views.my_account, name='my-account'),
+   path('history/', views.history, name='history'),
+   path('faq/', views.faq, name='faq'),
+   path('book-now-slc/', views.book_now_slc, name='slc'),
+   path('book-now-st-george/', views.book_now_st_george, name='st_george')
 ]
--- BeautyAndBush/webapp/views.py
+++ BeautyAndBush/webapp/views.py
@@ -1,10 +1,82 @@
 from django.shortcuts import render
 
-<<<<<<< Updated upstream
-# Create your views here.
-=======
+
+
+
 def MoodBoardView(request):
-    return render(request, 'webapp/moodboard.html')
+   return render(request, 'webapp/moodboard.html')
 
 
->>>>>>> Stashed changes
+
+
+def index(request):
+   return render(request, 'webapp/index.html')
+
+
+
+
+def about(request):
+   return render(request, 'webapp/about.html')
+
+
+
+
+def services(request):
+   return render(request, 'webapp/services.html')
+
+
+
+
+def memberships(request):
+   return render(request, 'webapp/memberships.html')
+
+
+
+
+def contact_us(request):
+   return render(request, 'webapp/contact_us.html')
+
+
+
+
+def login(request):
+   return render(request, 'webapp/login.html')
+
+
+
+
+def login(request):
+   return render(request, 'webapp/login.html')
+
+
+
+
+def my_account(request):
+   return render(request, 'webapp/my_account.html')
+
+
+
+
+def history(request):
+   return render(request, 'webapp/history.html')
+
+
+
+
+def faq(request):
+   return render(request, 'webapp/faq.html')
+
+
+
+
+def book_now_slc(request):
+
+
+   return render(request, 'webapp/book_now_slc.html')
+
+
+
+
+def book_now_st_george(request):
+   return render(request, 'webapp/book_now_st_george.html')
+
